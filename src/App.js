7a58d3e17bc832df45a35a5a747454f575c275b0
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2024-04-23 16:18:10 +0500

    midlizing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import Card from "./Components/Cards";
 import onlineData from "./Components/OnlineData";
+import { SiAirbnb } from "react-icons/si";
 
 console.log(onlineData)
 function App() {
   return (
-    <div className="w-full">
-    {onlineData.map((value) =>(
-      <div  key={value.id} > 
-      <Card value={value}/>
-      </div>
-    ))}
+    <div className="flex justify-center ">
+    <div className="flex items-center w-[55%] h-14  mt-10 border border-solid border-black bg-gray-200">
+        <div className=" flex  w-[100%] rounded-t-lg bg-red-200 text-red-600 font-bold font-mono h-14"> <h1 className="flex ml-7 items-center"><SiAirbnb size={22}/>airbnb</h1></div>
+        {onlineData.map((value) =>( 
+        <div  key={value.id} >
+        <Card value={value}/>
+        </div>
+        ))}
     </div>
+    </div>
+    
   );
 }
 
