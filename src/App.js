e88abc541d0f7798c3bb11ff64d886e971350036
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2024-04-24 09:53:41 +0500

    settling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import Card from "./Components/Cards";
-import onlineData from "./Components/OnlineData";
+// App.js
+import React from "react";
+import Card from "../src/Components/Cards";
+import onlineData from "../src/Components/OnlineData";
 import { SiAirbnb } from "react-icons/si";
 
-console.log(onlineData)
 function App() {
   return (
-    <div className="flex justify-center ">
-    <div className="flex items-center w-[55%] h-14  mt-10 border border-solid border-black bg-gray-200">
-        <div className=" flex  w-[100%] rounded-t-lg bg-red-200 text-red-600 font-bold font-mono h-14"> <h1 className="flex ml-7 items-center"><SiAirbnb size={22}/>airbnb</h1></div>
-        {onlineData.map((value) =>( 
-        <div  key={value.id} >
-        <Card value={value}/>
+    <div className="flex justify-center">
+      <div className="flex absolute items-center w-[55%] h-16 rounded-t-lg bg-orange-300 shadow-lg text-red-600 font-extrabold font-mono  ">
+          <SiAirbnb size={22} className="ml-10 "/>
+          <h1 className="ml-2 ">airbnb</h1>
         </div>
+      <div className=" mt-[66px] rounded-t-lg w-[55%] border border-solid border-black bg-gray-200">
+      
+        {onlineData.map((value) => (
+          <div key={value.id}>
+            <Card value={value}/>
+          </div>
         ))}
+      </div>
     </div>
-    </div>
-    
   );
 }
 
