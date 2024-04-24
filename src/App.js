9598cad32fee3b31106f114e45b1069c075360c6
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2024-04-24 15:29:03 +0500

    challanging

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ import { SiAirbnb } from "react-icons/si";
 function App() {
   return (
     <div className="flex justify-center">
-      <div className="flex absolute items-center  w-[80%] h-16 rounded-t-lg bg-orange-300 shadow-lg text-red-600 font-extrabold font-mono  ">
-          <SiAirbnb size={28} className="ml-10 "/>
-          <h1 className="ml-2 text-2xl ">airbnb</h1>
+      <div className="flex absolute items-center  w-[80%] h-16 rounded-t-lg bg-orange-600 shadow-lg text-red-600 font-extrabold font-mono  ">
+          <SiAirbnb size={28} className="ml-10 text-white"/>
+          <h1 className="ml-2 text-2xl text-white ">airbnb</h1>
         </div>
-      <div className="flex flex-wrap  mt-[66px] rounded-t-lg w-[80%] border border-solid border-black bg-white">
-      <div className=" ml-16 mt-12 w-[100%]">
-       <p className=" text-3xl font-black font-mono">Online Experiences
+      <div className="flex flex-wrap  mt-[57px] rounded-t-lg w-[80%] border border-solid shadow-lg bg-white mb-10">
+      <div className="ml-16 mt-12 w-[100%]">
+       <p className=" text-3xl font-black font-mono ">Online Experiences
        </p>
        
-       <p className="mt-3">Joint unique interactive activities led by one-of-a-kind  hosts-all without leaving home.</p>
+       <p className="mt-3 text-lg font-sans
+">Joint unique interactive activities led by one-of-a-kind  hosts-all without leaving home.</p>
        </div>
 
         {onlineData.map((value) => (
--- src/Components/OnlineData.jsx
+++ src/Components/OnlineData.jsx
@@ -2,6 +2,7 @@ const onlineData = [
     {
         id: 0,
         imagesource: "https://images.pexels.com/photos/6455772/pexels-photo-6455772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        tag: "SOLD OUT",
         rating:"5(6) .Online",
         description:"Health and Fitness Education",
         price: "From $50 / Person",
@@ -18,6 +19,7 @@ const onlineData = [
     {
         id: 2,
         imagesource: "https://paraglidinginfethiye.com/wp-content/uploads/2017/12/paragliding-fly.webp",
+        tag: "ONLINE",
         rating:"4.2(12) .GB Pakistan",
         description:"Learn to Paraglide!",
         price: "From $40/ Person",
@@ -36,12 +38,13 @@ const onlineData = [
         imagesource: "https://img.freepik.com/free-photo/person-taking-driver-s-license-exam_23-2149891379.jpg?t=st=1713944516~exp=1713948116~hmac=b658b29ba43013524fa6cec40a2fbf7bab2a2266b205aec8211af289a3735d35&w=996",
         rating:"4.8(5) .UAE",
         description:"Car Driving Classes",
-        price: "From $30/ Person",
+        price: "From $5000/ Person",
     },
 
     {
         id: 5,
         imagesource: "https://img.freepik.com/free-photo/homemade-pickles-jar_140725-3426.jpg?t=st=1713939619~exp=1713943219~hmac=732a2a87cef7488c2e5d3d65e90df0cb4296f786f636474e7a60de4fef569cd2&w=826",
+        tag: "SOLD OUT",
         rating:"5(100) .Pakistan",
         description:"Khala Ka Achar",
         price: "From $35/ Person",
